2d7f76bb49c2c9ba009abd0f554deb85ee483787
verti <verti@DESKTOP-1CBQMG9>
2024-03-14 22:03:17 -0600

    Init

---
+++ BlazorFormValidation/SupportedPassword.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormValidation;
+
+public class SupportedPassword : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var password = (string)value;
+        if (password == "password123")
+        {
+            return new ValidationResult("You cannot use the insecure password 'password123'.", new[] { validationContext.MemberName });
+        }
+
+        return ValidationResult.Success!;
+    }
+}
